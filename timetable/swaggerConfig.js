2ad2a85649185ede2c172d77ad0bb0fd6ec3a662
MaciejDubowik <maciejxpoland@gmail.com>
2024-01-17 20:42:22 +0100

    ts lab

---
+++ timetable/swaggerConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    info: {
+        title: 'Nazwa Twojego API',
+        version: '1.0.0',
+        description: 'Opis Twojego API',
+    },
+    host: 'localhost:3000', // Zmodyfikuj, jeśli inny port
+    basePath: '/',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+};
